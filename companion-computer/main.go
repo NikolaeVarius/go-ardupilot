67d4176cbdb578c2bda355c72bdfff5268ec0ab5
Joe Kim <joekim29@gmail.com>
2019-01-20 20:30:26 -0500

    intial commit trying out opencv

--- companion-computer/main.go
+++ companion-computer/main.go
@@ -1,27 +1,53 @@
 package main
 
 import (
-	"gobot.io/x/gobot"
-	"gobot.io/x/gobot/platforms/opencv"
+	"fmt"
+	"image"
+	"image/color"
+	// "math"
+
 	"gocv.io/x/gocv"
+	// "gocv.io/x/gocv/contrib"
 )
 
+const MinimumArea = 3000
+
 func main() {
-	window := opencv.NewWindowDriver()
-	camera := opencv.NewCameraDriver(0)
+	deviceID := 0
 
-	work := func() {
-		camera.On(opencv.Frame, func(data interface{}) {
-			img := data.(gocv.Mat)
-			window.ShowImage(img)
-			window.WaitKey(1)
-		})
+	webcam, err := gocv.OpenVideoCapture(deviceID)
+	if err != nil {
+		fmt.Printf("Error opening video capture device: %v\n", deviceID)
+		return
 	}
+	defer webcam.Close()
 
-	robot := gobot.NewRobot("cameraBot",
-		[]gobot.Device{window, camera},
-		work,
-	)
+	window := gocv.NewWindow("Hand Gestures")
+	defer window.Close()
 
-	robot.Start()
+	img := gocv.NewMat()
+	defer img.Close()
+
+	fmt.Printf("Start reading device: %v\n", deviceID)
+	for {
+		if ok := webcam.Read(&img); !ok {
+			fmt.Printf("Device closed: %v\n", deviceID)
+			return
+		}
+		if img.Empty() {
+			continue
+		}
+
+		status := fmt.Sprintf("Testing")
+
+		white := color.RGBA{255, 255, 255, 0}
+
+		gocv.PutText(&img, status, image.Pt(10, 20), gocv.FontHersheyPlain, 1.2, white, 2)
+
+		window.IMShow(img)
+		if window.WaitKey(1) == 27 {
+			break
+		}
+
+	}
 }
