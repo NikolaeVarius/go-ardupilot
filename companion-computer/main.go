4cb9db7d22006005dec1902f34420a5ca6bd4a13
Joe Kim <joekim29@gmail.com>
2019-04-28 13:34:19 -0400

    add facial recognition for analog video stream

--- companion-computer/main.go
+++ companion-computer/main.go
@@ -13,22 +13,32 @@ import (
 const MinimumArea = 3000
 
 func main() {
-	deviceID := 0
+	deviceID := 2
+	white := color.RGBA{255, 255, 255, 0}
+	blue := color.RGBA{0, 0, 255, 0}
+	webcam, err := gocv.VideoCaptureDevice(deviceID)
+	var haarCascadeFile = "../data/haarcascade_frontalface_default.xml"
 
-	webcam, err := gocv.OpenVideoCapture(deviceID)
 	if err != nil {
 		fmt.Printf("Error opening video capture device: %v\n", deviceID)
 		return
 	}
 	defer webcam.Close()
 
-	window := gocv.NewWindow("Hand Gestures")
+	window := gocv.NewWindow("Drone Video Out")
 	defer window.Close()
 
 	img := gocv.NewMat()
 	defer img.Close()
 
 	fmt.Printf("Start reading device: %v\n", deviceID)
+	classifier := gocv.NewCascadeClassifier()
+	defer classifier.Close()
+	if !classifier.Load(haarCascadeFile) {
+		fmt.Printf("Error reading cascade file: %v\n", haarCascadeFile)
+		return
+	}
+
 	for {
 		if ok := webcam.Read(&img); !ok {
 			fmt.Printf("Device closed: %v\n", deviceID)
@@ -38,11 +48,23 @@ func main() {
 			continue
 		}
 
-		status := fmt.Sprintf("Testing")
+		status := fmt.Sprintf("Scanning for Faces")
 
-		white := color.RGBA{255, 255, 255, 0}
+		// detect faces
+		rects := classifier.DetectMultiScale(img)
+		fmt.Printf("Found %d Faces\n", len(rects))
 
 		gocv.PutText(&img, status, image.Pt(10, 20), gocv.FontHersheyPlain, 1.2, white, 2)
+
+		// draw a rectangle around each face on the original image,
+		// along with text identifying as "Human"
+		for _, r := range rects {
+			gocv.Rectangle(&img, r, blue, 3)
+
+			size := gocv.GetTextSize("Human", gocv.FontHersheyPlain, 1.2, 2)
+			pt := image.Pt(r.Min.X+(r.Min.X/2)-(size.X/2), r.Min.Y-2)
+			gocv.PutText(&img, "Human", pt, gocv.FontHersheyPlain, 1.2, blue, 2)
+		}
 
 		window.IMShow(img)
 		if window.WaitKey(1) == 27 {
